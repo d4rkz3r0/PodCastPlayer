36e62860c6b3aa8b76689a67f9c99affab5dcda2
d4rkz3r0 <MacBookPro@local.com>
2017-07-22 23:47:50 -0400

    Initial Commit

---
+++ PodCastPlayer/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  PodCastPlayer
+//
+//  Created by Steve Kerney on 7/22/17.
+//  Copyright © 2017 d4rkz3r0. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
